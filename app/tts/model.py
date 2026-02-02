0bcba3758d5665161a61e9b02358d30cb3e14270
gyuhyeong <kgh0730@gmail.com>
2026-02-02 23:29:25 +0900

    Feat: Qwen3 TTS 모델 초기화 및 캐싱 기능 추가
    - Qwen3TTSModel을 사용하여 TTS 모델을 초기화함
    - lru_cache를 이용해 모델 캐싱 기능을 추가함

---
+++ app/tts/model.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+from qwen_tts import Qwen3TTSModel
+
+model = Qwen3TTSModel.from_pretrained(
+    "Qwen/Qwen3-TTS-12Hz-1.7B-Base",
+)
+
+
+@lru_cache
+def get_tts_model() -> Qwen3TTSModel:
+    return model
