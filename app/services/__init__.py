4ff5104817ca48af7e74cf43d07a4fb621cea5d3
gyuhyeong <kgh0730@gmail.com>
2026-02-03 23:20:48 +0900

    Feat: 음성 생성 기능 추가 및 서비스 패키지 업데이트
    - 음성 생성 관련 함수인 text_to_speech_generation과 batch_text_to_speech_generation을 추가함
    - __all__ 리스트에 새로운 함수들을 포함시켜 서비스 패키지를 업데이트함

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,13 @@
 """Initialize services package."""
 
 from .voice_clone import save_voice_clone
+from .voice_generate import (
+    batch_text_to_speech_generation,
+    text_to_speech_generation,
+)
 
-__all__ = ["save_voice_clone"]
+__all__ = [
+    "save_voice_clone",
+    "text_to_speech_generation",
+    "batch_text_to_speech_generation",
+]
